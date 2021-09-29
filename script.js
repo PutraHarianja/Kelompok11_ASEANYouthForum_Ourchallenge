3376dc7dd12526b129820faee341d2c151617126
Putra Harianja <putra.harianja007@gmail.com>
2021-09-29 15:20:54 +0700

    challange page

---
+++ script.js
@@ -0,0 +1,39 @@
+function myTextIsu() {
+    document.getElementById("text").innerHTML = "AYO berfokus pada isu-isu yang paling memprihatinkan untuk pemuda di Asia Tenggara. Oleh karena itu, kami menyediakan platform bagi mereka untuk menyampaikan keprihatinan mereka secara langsung. Tanpa saluran yang tepat bagi kaum muda untuk menyuarakan pendapat mereka, pandangan dan praktik yang sudah ketinggalan zaman akan berpotensi membahayakan kaum muda saat ini. Kami percaya bahwa dengan memastikan penanganan yang tepat dari masalah pemuda dan hak-hak pemuda kepada pembuat kebijakan yang relevan, ini adalah salah satu cara bagi kami untuk memastikan keberadaan masa depan kita semua.<br> Sebagai generasi yang akan menjadi pemimpin, kami ingin memastikan bahwa pemuda saat ini memiliki saluran untuk berpartisipasi dan memobilisasi bakat, ide, dan keterampilan mereka untuk menciptakan masyarakat yang lebih baik bagi kita semua.";
+
+    element.classList.add("active");
+}
+
+function myTextPerm() {
+    document.getElementById("text").innerHTML = "Karena isu pandemi, pemanasan global, dan perubahan iklim sangat mendesak, platform untuk memungkinkan kolaborasi yang berkelanjutan dan berdampak antara masyarakat sipil di ASEAN juga menjadi kebutuhan. Tidak hanya itu, saat ini kami mengalami masalah terkait pelacakan orientasi dan penyelesaian proses uji tuntas kelayakan program untuk setiap pelamar karena kami memiliki 3 tingkatan sebagai berikut :<br><ol><li>Penyandang disabilitas yang bukan bagian dari LSM atau organisasi penyandang disabilitas - Ini adalah salah satu masalah yang lebih berat dan membutuhkan lebih banyak privasi data. Karena kami memerlukan beberapa bukti untuk mengonfirmasi bahwa mereka adalah penyandang disabilitas, bukan penipu yang ingin memanfaatkan program atau dukungan yang kami berikan kepada penyandang disabilitas.</li><li>Penyandang disabilitas yang merupakan bagian dari LSM atau organisasi penyandang disabilitas - Beberapa dari mereka memiliki klausul privasi data di mana kita mungkin memiliki perantara dari organisasi tersebut sebagai orang utama untuk mengumpulkan individu penyandang disabilitas. Siapa yang mungkin tertarik dengan program yang mungkin kami tawarkan? Ini menghasilkan komunikasi dua arah untuk mengumpulkan jumlah individu yang tertarik atau memiliki detail kontak sehingga kami dapat menghubungi mereka.</li><li>Individu non-disabilitas - Ini mungkin memerlukan jenis dukungan yang berbeda tergantung pada motivasi dan prioritas mereka dalam hidup. Misalnya belajar sepanjang hayat, relevansi dengan pekerjaannya, memulai bisnis/usaha sampingan sendiri, perubahan pertengahan karir hingga masuk ke industri teknologi.</li></ul><br>Hal ini menyebabkan relawan kami menghabiskan waktu dan upaya untuk menjawab pertanyaan dan membuat email untuk meminta bukti, menelepon atau mengirim pesan teks ke orang-orang ini untuk informasi lebih lanjut tentang orang-orang ini.";
+
+    element.classList.add("active");
+}
+
+function myTextUsaha() {
+    document.getElementById("text").innerHTML = `Organisasi Internasional sudah ada, tetapi mereka tidak memiliki hubungan dengan organisasi grassroot dan kurangnya kecakapan bahasa Inggris di beberapa negara, terutama di daerah pedesaan cukup menyulitkan usaha yg ada. Kurangnya rasa memiliki khususnya di kawasan ASEAN membuat semakin sulit untuk menciptakan visi dan identitas yang sama; dengan demikian, hal tersebut menciptakan kesenjangan antara para pemangku kepentingan. Pemerintah menyadari masalah dan memberikan solusi, namun kesadaran untuk mengatasi masalah sebagai satu komunitas belum ada.
+    <br><br>
+    Dalam mendukung teman-teman penyandang disabilitas, penggunaan chatbot siap pakai dengan CRM dan solusi low code dapat diterapkan, tetapi implementasinya menjadi masalah karena kami memerlukan integrasi media sosial dan email untuk mengurangi waktu bagi para sukarelawan dan administrator.
+    <br><br>
+    Apa yang saat ini dilakukan adalah menggunakan google excel untuk melacak dan memperbarui excel untuk penyandang disabilitas dan individu non-disabilitas. Saat ini, karena kurangnya sumber daya dan waktu, kami tidak dapat menerapkannya sendiri.
+    <br><br>
+    Kamu diperbolehkan melakukan eksplorasi dan membuat asumsi sendiri di luar dari detail challenge yang diberikan. Sebagai contoh, jika challenge tidak menerangkan lebih detail mengenai target pengguna, maka kamu bisa asumsikan sendiri profil pengguna yang berpotensi menggunakan produk dari partner. Tidak hanya itu, kamu juga bisa bereksplorasi untuk menghasilkan flow produk digital yang lebih baik jika dibutuhkan.`;
+    
+    element.classList.add("active");
+}
+
+function myTextTujuan() {
+    document.getElementById("text").innerHTML = `Buatlah sebuah solusi ataupun inovasi digital dalam bentuk prototype, MVP ataupun pitch deck yang dapat menjawab beberapa pertanyaan berikut berdasarkan permasalahan yang diuraikan sebelumnya :<br>
+    <ul>
+        <li>Bagaimana kita bisa terhubung dengan organisasi grassroot?</li>
+        <li>Bagaimana kita bisa menghancurkan hambatan bahasa serta budaya untuk berkomunikasi lebih baik dengan pemuda di seluruh dunia?</li>
+        <li>Bagaimana kita bisa meningkatkan kesadaran identitas regional serta identitas global untuk mengatasi masalah bersama?</li>
+        <li>Bagaimana kita dapat mengatasi masalah orientasi dalam proses uji tuntas penyandang disabilitas yang mungkin memenuhi syarat untuk program kami?</li>
+        <li>Bagaimana kita dapat menjawab pertanyaan yang diajukan oleh individu untuk memilih program dan dukungan yang tepat untuk mereka?</li>
+        <li>Bagaimana kita dapat mengurangi waktu dan upaya untuk melacak berbagai individu selama proses orientasi untuk program?</li>
+    </ul>
+    Kamu dapat menggunakan software rekomendasi kami sebagai referensi atau alat bantu untuk menjawab tantangan di atas. Jika kamu ingin upload yang file yang dibutuhkan untuk menjawab tugas proyek, silahkan gunakan Google Drive dan berikan akses link.`;
+    
+    var element = document.getElementsByClassName("tujuan")
+    element.classList.add("active")
+}
